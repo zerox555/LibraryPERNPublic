c0f58156ec3442459a53e994b338a8d4607fffab
Jing En Lau <laujingen@gmail.com>
2024-11-14 13:21:58 +0800

    added functional react dev server

---
+++ src/components/Library.jsx
@@ -0,0 +1,38 @@
+import { useState, useEffect } from "react"
+
+
+export default function Library() {
+    //declare list of stateful list 'books' and function 'setBooks' to update
+    const [ books, setBooks ] = useState([])
+
+    //only update if the value changes
+    useEffect(() => {
+        const fetchData = async () => {
+            const response = await fetch("http://localhost:4005/api/books")
+            const json = await response.json()
+            setBooks(json)
+        }
+        fetchData()
+    }, []);
+
+    return (
+        <div>
+            <h1>Library</h1>
+            <ul>
+                {books.map((book, index) => (
+                    <li key={index} style = {{paddingBottom: '25px'}}>
+                        <div>{book.name}</div>
+                        <div>{book.author}</div>
+                        <div>{book.year_published}</div>
+
+                    </li>
+                ))}
+
+            </ul>
+  
+            <ul>
+                dsfsd
+            </ul>
+        </div>
+    )
+}
