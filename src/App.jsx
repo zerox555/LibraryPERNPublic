39321c426a81c10c001f02de2054a603528c9db5
Jing En Lau <laujingen@gmail.com>
2024-11-28 16:10:31 +0800

    Removed used state in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import './App.css';
-import { useState, useEffect } from "react";
 import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom"
 import Library from './components/Library';
 import Navbar from './components/Navbar';
@@ -7,8 +6,7 @@ import Home from './components/Home';
 import Login from './components/Login';
 
 function App() {
-  //login state
-  const [loggedIn, setLoggedIn] = useState(0)
+
 
   return (
     <div className="container">
@@ -17,12 +15,6 @@ function App() {
 
         <Routes>
           <Route path="/" element={<Navigate to="/home" />}></Route>
-          {/* If not logged in redirect to the login page */}
-
-          {/* {loggedIn === 1 ? (
-             />
-          ) : } */}
-          {/* <Route path='/library' element={<Navigate to="/login" />}> </Route> */}
           <Route path='/library' element={<Library />} />
           <Route path='/login' element={<Login />} />
           <Route path='/home' element={<Home />} />
