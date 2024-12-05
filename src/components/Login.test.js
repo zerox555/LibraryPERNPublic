04659d6ce260187f1e2dc424704ce6dec336cc58
Jing En Lau <laujingen@gmail.com>
2024-12-05 14:40:05 +0800

    Added another test case

--- src/components/Login.test.js
+++ src/components/Login.test.js
@@ -1,8 +1,6 @@
 import { render, screen, fireEvent, waitFor } from '@testing-library/react';
 import Login from './Login';
-// Test user login assuming wrong credentials 
 
-// Test user submits form with empty details
 
 // Test login page is correctly rendered
 test("Login page all elements load correctly", () => {
@@ -48,7 +46,7 @@ test('Server not accessable', async () => {
 });
 
 // Test user login assuming correct credentials
-test('Assuming user credentials correct, allows the user to login', async () => {
+test('Allows user login if correct credentials', async () => {
 
     // Mock the global fetch function for a successful response
     global.fetch = jest.fn(() =>
@@ -86,7 +84,7 @@ test('Assuming user credentials correct, allows the user to login', async () => 
 });
 
 // Test user login assuming wrong credentials 
-test('Reject login user if incorrect credentials', async () => {
+test('Reject user login if incorrect credentials', async () => {
 
     // Mock the global fetch function for a successful response
     global.fetch = jest.fn(() =>
@@ -121,4 +119,28 @@ test('Reject login user if incorrect credentials', async () => {
     expect(loginButton).toBeInTheDocument();
 
     await waitFor(() => expect(global.alert).toHaveBeenCalledWith("Failed to login user"));
+});
+
+// Test user submits form with empty details
+test('Prevent user submit form with empty details', async () => {
+
+
+
+    // Render the Login component
+    render(<Login setLoggedIn={jest.fn()} setToken={jest.fn()} />);
+
+    // Mock the alert function
+    global.alert = jest.fn();
+
+    // Get the username and password fields and submit button
+    const usernameField = screen.getByLabelText(/username/i);
+    const passwordField = screen.getByLabelText(/password/i);
+    const loginButton = screen.getByRole('button', { name: "Login" });
+
+    // Simulate form submission
+    fireEvent.click(loginButton);
+
+    expect(usernameField).toBeInvalid();
+    expect(passwordField).toBeInvalid();
+
 });
