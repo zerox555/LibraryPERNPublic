45d882f156f7c2fe31bc2dba26951e864e21dd29
Jing En Lau <laujingen@gmail.com>
2024-12-06 15:40:58 +0800

    Added logging levels to book,role,user_service

--- server/services/role_service.js
+++ server/services/role_service.js
@@ -1,35 +1,58 @@
 const fs = require('fs');
 const path = require('path');
+const logger = require("../config/logger")
+require('dotenv').config();
 
 let rolesData;
 
 const loadRoles = () => {
-    if (!rolesData) {
-        const rolesFile = path.join(__dirname, '../config/roles.json');
-        const data = fs.readFileSync(rolesFile, 'utf8');
-        rolesData = JSON.parse(data).roles;
+    try {
+        if (!rolesData) {
+            const rolesFile = path.join(__dirname, '../config/roles.json');
+            const data = fs.readFileSync(rolesFile, 'utf8');
+            rolesData = JSON.parse(data).roles;
+            logger.info(`Got Roles data from roles.json: ${JSON.stringify(rolesData)}`)
+        }
+    } catch (err) {
+        logger.error(`Error loading roles in loadRoles function: ${err}`);
     }
+
     return rolesData;
 };
 
 const getPermissionsByRole = (allRoles) => {
-    const roles = loadRoles();
-    if (allRoles.length > 1) {
-        //loop multiple times and get all roles
-        let role = [];
-        for (let i = 0; i < allRoles.length; i++) {
-            const currrentRole = roles.find((r) => r.name === allRoles[i]);
-            // Check if element exists
-            if (role.includes(currrentRole.permissions)) {
-                // Skip
+    try {
+        const roles = loadRoles();
+        logger.trace(`allRoles length: ${allRoles.length}`)
+        if (roles) {
+            if (allRoles.length > 1) {
+                //loop multiple times and get all roles
+                let role = [];
+                for (let i = 0; i < allRoles.length; i++) {
+                    const currrentRole = roles.find((r) => r.name === allRoles[i]);
+                    // Check if element exists
+                    if (role.includes(currrentRole.permissions)) {
+                        // Skip
+                    } else {
+                        role.push(currrentRole.permissions)
+                    }
+                    logger.warn(`Got permission data from multiple roles: ${JSON.stringify(role)}`)
+                    return role ? role.permissions : [];
+                }
             } else {
-                role.push(currrentRole.permissions)
+                const role = roles.find((r) => r.name === allRoles[0]);
+                logger.info(`Got permission data from single role: ${JSON.stringify(role)}`)
+                return role ? role.permissions : [];
             }
+        } else {
+            logger.debug("returned []");
+            return [];
         }
-    } else {
-        const role = roles.find((r) => r.name === allRoles[0]);
-        return role ? role.permissions : [];
+
+    } catch (err) {
+        logger.error(`Error getting permissions in getPermissionsByRole: ${err}`);
     }
+
 };
 
 module.exports = {
--- server/services/user_service.js
+++ server/services/user_service.js
@@ -3,6 +3,8 @@ const { User } = db
 const argon2 = require('argon2');
 const jwt = require("jsonwebtoken");
 const { getPermissionsByRole } = require("./role_service");
+const logger = require("../config/logger")
+require('dotenv').config();
 
 // ADD NEW USER TO DB
 const create_user_post = async (userData) => {
@@ -14,6 +16,7 @@ const create_user_post = async (userData) => {
             roles: ["user"]
         }
         );
+        logger.info(`Created User: ${userData.name} with roles: user`);
         return newUser;
     } catch (err) {
         Alert("Error creating user: " + err.message);
@@ -23,6 +26,7 @@ const create_user_post = async (userData) => {
 // AUTHENTICATE USER
 const auth_user = async (userData) => {
     let token;
+    let permissions;
     try {
         let authenticated = false;
         const user = await User.findOne({
@@ -31,12 +35,19 @@ const auth_user = async (userData) => {
             }
         })
 
-        if (user && (await argon2.verify(user.password, userData.password)) ) {
+
+        if (user && (await argon2.verify(user.password, userData.password))) {
             authenticated = true
+            logger.info(`Found User with username: ${user.name}`)
+            logger.info("User authentication success");
             //set jwt token here 
             try {
                 //Creating JWT token
                 permissions = getPermissionsByRole(user.roles);
+<<<<<<< HEAD
+=======
+                logger.debug(`permissions length: ${permissions.length}`);
+>>>>>>> 57d7ad4 (Added logging levels to book,role,user_service)
                 if (permissions.length != 0) {
                     token = jwt.sign(
                         {
@@ -49,26 +60,46 @@ const auth_user = async (userData) => {
                         process.env.REACT_APP_JWT_SECRET,
                         { expiresIn: "1h" }
                     )
+<<<<<<< HEAD
                 }
+=======
+                    logger.debug(`JWT token created: ${token}`);
+                    logger.info("JWT token created");
+                }
+
+
+
+>>>>>>> 57d7ad4 (Added logging levels to book,role,user_service)
             } catch (err) {
-                console.log(err);
+                logger.error("Error creating JWT token");
                 const error =
                     new Error("Error! Something went wrong.");
                 return next(error);
             }
         }
         else {
+            //invalid login
+            logger.warn(`Invalid login detected with username: ${userData.name}`);
         }
+<<<<<<< HEAD
 
         if (token && permissions.length > 0) {
             return {
                 success: authenticated,
                 data: authenticated
                     ? {
+=======
+        logger.debug(`User authenticated: ${authenticated}`);
+        return (
+            (authenticated && token && permissions.length != 0) ?
+                {
+                    success: authenticated,
+                    data: {
+>>>>>>> 57d7ad4 (Added logging levels to book,role,user_service)
                         id: user.id,
                         name: userData.name,
                         roles: user.roles,
-                        permissions: getPermissionsByRole(user.roles),
+                        permissions: permissions,
                         token: token,
                     }
                     : { errorMsg: "Error logging user in!" },
@@ -83,7 +114,8 @@ const auth_user = async (userData) => {
         };
 
     } catch (err) {
-        console.log(err)
+        logger.error(`Error occured: ${err}`);
+        throw new Error(err);
     }
 };
 
