27cd87f9f9e607583337e4f0430429734e815953
Jing En Lau <laujingen@gmail.com>
2024-12-06 14:07:02 +0800

    Added working pino,pino-pretty packages to configure loggin

--- server/controllers/user_controller.js
+++ server/controllers/user_controller.js
@@ -1,10 +1,22 @@
 const { create_user_post, auth_user } = require("../services/user_service");
+const pino = require('pino');
+
+const logger = pino({
+    transport: {
+      target: 'pino-pretty',
+      options: {
+        colorize: true
+      }
+    }
+  });
+  
 
 // ADD NEW USER
 const create_user_post_controller = async (req, res) => {
     try {
         const { name, password } = req.body;
         const newUser = await create_user_post({ name, password });
+        logger.info("New User created");
         res.status(200).send(newUser);
     } catch (err) {
         res.status(500).send("Server error");
